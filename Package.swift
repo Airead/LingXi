b0f0ea3a35a585d784e6ba5b1e191a6ad13db1e2
Airead <fgh1987168@gmail.com>
2026-04-03 09:29:22 +0800

    fix: bump Package.swift macOS target to v14 for SourceKit-LSP compatibility
    Resolves onChange(of:initial:_:) availability warning in VS Code.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "LingXi",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     targets: [
         .target(
             name: "LingXi",
